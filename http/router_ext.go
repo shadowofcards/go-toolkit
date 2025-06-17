09b6e97f38f40052ee101241ff18fbe4678d34ce
Leandro Ferreira <leandro.daf4@gmail.com>
2025-06-17 08:59:00 -0300

    refactor: update RouterProtected function to accept flexible method types and improve permissions handling

--- http/router_ext.go
+++ http/router_ext.go
@@ -1,24 +1,39 @@
 package http
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v3"
 )
 
-func RegisterProtected(
+func RouterProteced(
 	r fiber.Router,
-	methods []string,
+	methodsArg interface{},
 	path string,
 	handler fiber.Handler,
-	permissions ...string,
+	permissions ...any,
 ) fiber.Router {
+
+	var methods []string
+	switch m := methodsArg.(type) {
+	case string:
+		methods = []string{m}
+	case []string:
+		methods = m
+	default:
+		panic(fmt.Sprintf("RouterProteced: methods must be string or []string, got %T", methodsArg))
+	}
+
 	if len(permissions) == 0 {
 		return r.Add(methods, path, handler)
 	}
 
-	var mws []fiber.Handler
-	for _, perm := range permissions {
-		mws = append(mws, RequirePermission(perm))
+	mws := make([]fiber.Handler, 0, len(permissions)+1)
+	for _, p := range permissions {
+		permStr := fmt.Sprint(p)
+		mws = append(mws, RequirePermission(permStr))
 	}
+	mws = append(mws, handler)
 
-	return r.Add(methods, path, handler, mws...)
+	return r.Add(methods, path, mws[0], mws[1:]...)
 }
